ab39b1a73edc190d8f5ba3e869448e58649bdef8
Aryan Kanak <akanak314@gmail.com>
2024-03-10 12:32:45 -0400

    Updated build once more

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "/psych-253-group-assignment/"
+  base: "/psych-253-assignment/"
 })
